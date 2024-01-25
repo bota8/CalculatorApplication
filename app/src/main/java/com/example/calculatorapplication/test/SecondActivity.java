d66117b3d84ca0d0545b805c5b995d0977dc2388
bota <katniss8346@gmail.com>
2024-01-25 20:23:00 +0600

    First commit Added 1 Constructor and 2 methods

--- app/src/main/java/com/example/calculatorapplication/test/SecondActivity.java
+++ app/src/main/java/com/example/calculatorapplication/test/SecondActivity.java
@@ -1,4 +1,15 @@
 package com.example.calculatorapplication.test;
 
 public class SecondActivity {
+    public SecondActivity() {
+    }
+
+    int test1() {
+        return 1;
+    }
+    int test2() {
+        return 88;
+    }
+
+
 }
