efebe87ed87636602fd4cefbbb5c06b7591f280d
Bota <zhanbota.adilbek@gmail.com>
2024-01-25 20:56:19 +0600

    Added FourthActivity

---
+++ app/src/main/java/com/example/calculatorapplication/test/FourthActivity.java
@@ -0,0 +1,11 @@
+package com.example.calculatorapplication.test;
+
+public class FourthActivity {
+    String text;
+    int numb;
+
+    public FourthActivity(String text, int numb) {
+        this.text = text;
+        this.numb = numb;
+    }
+}
