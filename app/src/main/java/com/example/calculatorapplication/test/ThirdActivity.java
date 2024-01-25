d501bff160c95119db42bc27ea4421a7724d9837
bota <katniss8346@gmail.com>
2024-01-25 20:40:31 +0600

    First commit Added 1 Constructor and 2 methods and 1 ArrayList and class

--- app/src/main/java/com/example/calculatorapplication/test/ThirdActivity.java
+++ app/src/main/java/com/example/calculatorapplication/test/ThirdActivity.java
@@ -3,4 +3,16 @@ package com.example.calculatorapplication.test;
 public class ThirdActivity {
     String name;
     int numb;
+
+    public ThirdActivity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumb() {
+        return numb;
+    }
 }
